c50d1b2bd36814f20ca71b228779a25dba626ab2
Iftuga <iftuga@ya.ru>
2019-04-29 16:03:06 +0300

    Fixed users

--- SQL Files/users.sql
+++ SQL Files/users.sql
@@ -2,81 +2,81 @@
 ALTER SESSION SET "_ORACLE_SCRIPT" = true;
 
 -- Создание пользователей
-CREATE USER ts IDENTIFIED BY parol; -- сотрудники техподдержки
-CREATE USER wwd IDENTIFIED BY parol; -- сотрудники из отдела по работе с разработчиками
-CREATE USER wwc IDENTIFIED BY parol; -- сотрудники из отдела по работе с каталогом
-CREATE USER bk IDENTIFIED BY parol; -- бухгалтерия
-CREATE USER hr IDENTIFIED BY parol; -- отдел кадров
-CREATE USER cl IDENTIFIED BY parol; -- клиенты
+CREATE USER tech_support IDENTIFIED BY password1; -- сотрудники техподдержки
+CREATE USER work_with_dev IDENTIFIED BY password2; -- сотрудники из отдела по работе с разработчиками
+CREATE USER work_with_catalog IDENTIFIED BY password3; -- сотрудники из отдела по работе с каталогом
+CREATE USER bookkeeping IDENTIFIED BY password4; -- бухгалтерия
+CREATE USER human_res IDENTIFIED BY password5; -- отдел кадров
+CREATE USER customers IDENTIFIED BY password6; -- клиенты
 
 -- Техподержка
-GRANT SELECT ON products TO ts;
-GRANT SELECT ON products_types TO ts;
-GRANT SELECT ON language_reference_table TO ts;
-GRANT SELECT ON localization TO ts;
-GRANT SELECT ON programs TO ts;
-GRANT SELECT ON games TO ts;
-GRANT SELECT,INSERT,UPDATE,DELETE ON licenses TO ts;
-GRANT SELECT ON licenses_types TO ts;
-GRANT SELECT,INSERT,UPDATE,DELETE ON subscription_pay TO ts;
-GRANT SELECT,INSERT,UPDATE,DELETE ON customers TO ts;
-GRANT SELECT ON departs TO ts;
-GRANT SELECT ON employers TO ts;
-GRANT SELECT,INSERT,UPDATE ON requests TO ts;
-GRANT SELECT,INSERT,UPDATE,DELETE ON requests_themes TO ts;
-GRANT SELECT,INSERT,UPDATE,DELETE ON requests_statuses TO ts;
+GRANT SELECT ON products TO tech_support;
+GRANT SELECT ON products_types TO tech_support;
+GRANT SELECT ON language_reference_table TO tech_support;
+GRANT SELECT ON localization TO tech_support;
+GRANT SELECT ON programs TO tech_support;
+GRANT SELECT ON games TO tech_support;
+GRANT SELECT,INSERT,UPDATE,DELETE ON licenses TO tech_support;
+GRANT SELECT ON licenses_types TO tech_support;
+GRANT SELECT,INSERT,UPDATE,DELETE ON subscription_pay TO tech_support;
+GRANT SELECT,INSERT,UPDATE,DELETE ON customers TO tech_support;
+GRANT SELECT ON departs TO tech_support;
+GRANT SELECT ON employers TO tech_support;
+GRANT SELECT,INSERT,UPDATE ON requests TO tech_support;
+GRANT SELECT,INSERT,UPDATE,DELETE ON requests_themes TO tech_support;
+GRANT SELECT,INSERT,UPDATE,DELETE ON requests_statuses TO tech_support;
 
 -- Отдел по работе с разработчиками
-GRANT SELECT,INSERT,UPDATE,DELETE ON developers TO wwd;
-GRANT SELECT,INSERT,UPDATE,DELETE ON dev_phones TO wwd;
-GRANT SELECT ON products TO wwd;
-GRANT SELECT ON products_types TO wwd;
-GRANT SELECT ON language_reference_table TO wwd;
-GRANT SELECT ON localization TO wwd;
-GRANT SELECT ON programs TO wwd;
-GRANT SELECT ON games TO wwd;
-GRANT SELECT ON departs TO wwd;
-GRANT SELECT ON employers TO wwd;
+GRANT SELECT,INSERT,UPDATE,DELETE ON developers TO work_with_dev;
+GRANT SELECT,INSERT,UPDATE,DELETE ON dev_phones TO work_with_dev;
+GRANT SELECT ON products TO work_with_dev;
+GRANT SELECT ON products_types TO work_with_dev;
+GRANT SELECT ON language_reference_table TO work_with_dev;
+GRANT SELECT ON localization TO work_with_dev;
+GRANT SELECT ON programs TO work_with_dev;
+GRANT SELECT ON games TO work_with_dev;
+GRANT SELECT ON departs TO work_with_dev;
+GRANT SELECT ON employers TO work_with_dev;
 
 -- Отдел по работе с каталогом
-GRANT SELECT ON developers TO wwc;
-GRANT SELECT,INSERT,UPDATE,DELETE ON products TO wwc;
-GRANT SELECT,INSERT,UPDATE,DELETE ON products_types TO wwc;
-GRANT SELECT,INSERT,UPDATE,DELETE ON language_reference_table TO wwc;
-GRANT SELECT,INSERT,UPDATE,DELETE ON localization TO wwc;
-GRANT SELECT,INSERT,UPDATE,DELETE ON programs TO wwc;
-GRANT SELECT,INSERT,UPDATE,DELETE ON games TO wwc;
-GRANT SELECT,INSERT,UPDATE,DELETE ON licenses_types TO wwc;
-GRANT SELECT ON departs TO wwc;
-GRANT SELECT ON employers TO wwc;
+GRANT SELECT ON developers TO work_with_catalog;
+GRANT SELECT,INSERT,UPDATE,DELETE ON products TO work_with_catalog;
+GRANT SELECT,INSERT,UPDATE,DELETE ON products_types TO work_with_catalog;
+GRANT SELECT,INSERT,UPDATE,DELETE ON language_reference_table TO work_with_catalog;
+GRANT SELECT,INSERT,UPDATE,DELETE ON localization TO work_with_catalog;
+GRANT SELECT,INSERT,UPDATE,DELETE ON programs TO work_with_catalog;
+GRANT SELECT,INSERT,UPDATE,DELETE ON games TO work_with_catalog;
+GRANT SELECT,INSERT,UPDATE,DELETE ON licenses_types TO work_with_catalog;
+GRANT SELECT ON departs TO work_with_catalog;
+GRANT SELECT ON employers TO work_with_catalog;
 
 -- Бухгалтерия
-GRANT SELECT ON developers TO bk;
-GRANT SELECT ON dev_phones TO bk;
-GRANT SELECT ON products TO bk;
-GRANT SELECT ON products_types TO bk;
-GRANT SELECT ON programs TO bk;
-GRANT SELECT ON games TO bk;
-GRANT SELECT ON licenses TO bk;
-GRANT SELECT ON licenses_types TO bk;
-GRANT SELECT ON subscription_pay TO bk;
-GRANT SELECT ON customers TO bk;
-GRANT SELECT ON departs TO bk;
-GRANT SELECT ON employers TO bk;
-GRANT SELECT ON appointments TO bk;
+GRANT SELECT ON developers TO bookkeeping;
+GRANT SELECT ON dev_phones TO bookkeeping;
+GRANT SELECT ON products TO bookkeeping;
+GRANT SELECT ON products_types TO bookkeeping;
+GRANT SELECT ON programs TO bookkeeping;
+GRANT SELECT ON games TO bookkeeping;
+GRANT SELECT ON licenses TO bookkeeping;
+GRANT SELECT ON licenses_types TO bookkeeping;
+GRANT SELECT ON subscription_pay TO bookkeeping;
+GRANT SELECT ON customers TO bookkeeping;
+GRANT SELECT ON departs TO bookkeeping;
+GRANT SELECT ON employers TO bookkeeping;
+GRANT SELECT ON appointments TO bookkeeping;
 
 -- Отдел кадров
-GRANT SELECT,INSERT,UPDATE,DELETE ON departs TO hr;
-GRANT SELECT,INSERT,UPDATE,DELETE ON employers TO hr;
-GRANT SELECT,INSERT,UPDATE,DELETE ON appointments TO hr;
+GRANT SELECT,INSERT,UPDATE,DELETE ON departs TO human_res;
+GRANT SELECT,INSERT,UPDATE,DELETE ON employers TO human_res;
+GRANT SELECT,INSERT,UPDATE,DELETE ON appointments TO human_res;
 
 -- Клиенты
-GRANT SELECT ON products TO cl;
-GRANT SELECT ON products_types TO cl;
-GRANT SELECT ON localization TO cl;
-GRANT SELECT ON programs TO cl;
-GRANT SELECT ON games TO cl;
-GRANT SELECT,INSERT,UPDATE,DELETE ON customers TO cl;
-GRANT SELECT,INSERT ON requests TO cl;
-GRANT SELECT ON requests_themes TO cl;
-GRANT SELECT ON requests_statuses TO cl;
+GRANT SELECT ON products TO customers;
+GRANT SELECT ON products_types TO customers;
+GRANT SELECT ON localization TO customers;
+GRANT SELECT ON programs TO customers;
+GRANT SELECT ON games TO customers;
+GRANT SELECT,INSERT,UPDATE,DELETE ON customers TO customers;
+GRANT SELECT,INSERT ON requests TO customers;
+GRANT SELECT ON requests_themes TO customers;
+GRANT SELECT ON requests_statuses TO customers;
