85e1537fbe8c62508eaf44ec559c8f780b874136
Iftuga <iftuga@ya.ru>
2019-05-04 18:21:22 +0300

    New views

--- SQL Files/views.sql
+++ SQL Files/views.sql
@@ -5,3 +5,33 @@ CREATE VIEW devs_emails AS
         dev_email
     FROM
         developers;
+
+-- Список продуктов за последний месяц
+CREATE OR REPLACE VIEW recent_products AS
+    SELECT
+        prod_name
+    FROM
+        products
+    WHERE
+        prod_date >= SYSDATE - 30;
+
+-- Сотрудники мужчины
+CREATE OR REPLACE VIEW men_emp AS
+    SELECT
+        emp_name,
+        emp_sname,
+        emp_pname
+    FROM
+        employers
+    WHERE
+        emp_gender = 'м';
+-- Сотрудники женщины
+CREATE OR REPLACE VIEW men_emp AS
+    SELECT
+        emp_name,
+        emp_sname,
+        emp_pname
+    FROM
+        employers
+    WHERE
+        emp_gender = 'ж';
