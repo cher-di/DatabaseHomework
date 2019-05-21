0475dd4851211570c67d7f8b70eead5b7815658c
cher-di <cher-di@mail.ru>
2019-05-21 11:56:13 +0300

    Added two new views

--- SQL Files/views.sql
+++ SQL Files/views.sql
@@ -25,13 +25,28 @@ CREATE OR REPLACE VIEW men_emp AS
         employers
     WHERE
         emp_gender = 'м';
--- Сотрудники женщины
-CREATE OR REPLACE VIEW women_emp AS
-    SELECT
-        emp_name,
-        emp_sname,
-        emp_pname
-    FROM
-        employers
-    WHERE
-        emp_gender = 'ж';
+
+-- Сроки истечения платных подписок (до какого срока проплачена подписка)
+CREATE OR REPLACE VIEW subscription_expiration_date AS
+  SELECT
+      l.lic_id,
+      ADD_MONTHS(l.lic_date, SUM(s.sp_month_cnt)) AS lic_exp_date,
+  FROM
+      licenses l, subscription_pay s
+  WHERE
+      l.lic_id = s.sp_id,
+      l.lic_type = "подписка"
+  GROUP BY
+      l.lic_id;
+      
+-- Запросы к техподдержке, датированные сегодняшним днем
+CREATE OR REPLACE VIEW today_request AS
+  SELECT
+      req_id,
+      req_login,
+      req_theme,
+      req_emp_id,
+      req_descr,
+      req_status
+  FROM requests
+  WHERE TRUNC(SYSDATE) = TRUNC(req_date);
