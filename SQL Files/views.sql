80c52acd96d8c890083577d53c8440542436b4e1
cher-di <cher-di@mail.ru>
2019-05-21 15:45:43 +0300

    Fixed view

--- SQL Files/views.sql
+++ SQL Files/views.sql
@@ -28,14 +28,9 @@ CREATE OR REPLACE VIEW men_emp AS
 
 -- Сроки истечения платных подписок (до какого срока проплачена подписка)
 CREATE OR REPLACE VIEW subscription_expiration_date AS
-    SELECT
-        temp.lic_id
-        add_months(temp.lic_date,SUM(temp.sp_month_cnt) AS lic_exp_date
-    FROM
-        (SELECT * FROM licenses, subscription_pay WHERE lic_id = sp_id) temp
-    WHERE
-        temp.lic_type = 'т лиц'
-    GROUP BY temp.lic_id;
+    SELECT sp_id, SUM(sp_month_cnt)
+    FROM subscription_pay
+    GROUP BY sp_id;
       
 -- Запросы к техподдержке, датированные сегодняшним днем
 CREATE OR REPLACE VIEW today_request AS
